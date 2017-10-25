b2b915d2c9e9fe0d6c052ccbc9529440176bf9a8
abdulrahmantkhalifa <abdulrahman.t.khalifa@gmail.com>
2017-10-25 13:39:13 +0200

    defining permission groups if missing (#61)

--- actors/init.py
+++ actors/init.py
@@ -6,7 +6,20 @@ from cloudbroker.healthcheck import healthcheck
 
 
 def init():
+
+    # defining the persmission level groups if they are not created already
+    requiredLevels = {'level1', 'level2', 'level3'}
+    for group in j.portal.tools.models.system.Group.objects(name__startswith='level'):
+        if group.name in requiredLevels:
+            requiredLevels.remove(group.name)
+    for level in requiredLevels:
+        levelGroup = j.portal.tools.models.system.Group(name=level)
+        levelGroup.save()
+
+    # hooking the cloudbroker database(models) to j
     j.portal.tools.models.cloudbroker = Models()
+
+    # spawning the health checks and the stats collector(this should be move to orchestrator)
     health = healthcheck.Healthcheck()
     gevent.spawn(health.start)
     gevent.spawn(resourcemonitoring.collect_stats)
